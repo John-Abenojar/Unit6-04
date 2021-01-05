f028d230f5bfb5cdfb640821f5e22f972de2f5c5
JohnAbenojar <john.kirby.abenojar@ocsbstudent.ca>
2021-01-05 17:01:49 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var age = prompt("Enter your age")
+var day = "Wednesday"
+if ((day == "Thursday" || day == "Tuesday") || (age >=12 && age <=21)) {
+  alert ("You may enter the museum")
+}
+else {
+  alert ("You may not enter the museum")
+}
